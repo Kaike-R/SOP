1ed4026fb2118c368cf23c987886b333112f81b3
Kaike Rodrigues Zuanetti <49592480+Kaike-R@users.noreply.github.com>
2020-12-11 11:16:10 -0300

    Update diario.sh

--- diario.sh
+++ diario.sh
@@ -4,6 +4,16 @@ python cota.py
 notify-send -i face-laugh -t 3000 "$(cat ~/cota.txt)"
 
 touch log.txt
+touch date.txt
+touch plog.txt
+touch p.txt
+ps acu >> p.txt
+date >> date.txt
+cat date.txt >> log.txt
 cat cota.txt >> log.txt
+cat date.txt >> plog.txt
+cat p.txt >> plog.txt
+rm p.txt
+rm date.txt
 
 exit
